72d8bdf416c8f659b8eaf0e212cd54225bea26a8
HigashikataJosuke0 <149962024+HigashikataJosuke0@users.noreply.github.com>
2023-11-07 16:45:33 +0300

    Add files via upload

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -1,16 +1,18 @@
+import java.nio.channels.NetworkChannel;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
 
 
 public class Main {
     private static Scanner console = new Scanner(System.in);
-    private static int a, b;
+    private static int a, b, result;
     private static Boolean res = false;
 
     private static String[] data;
     private static HashMap<String, Integer> m = new HashMap<String, Integer>() {{
-        put("0",0);
+        put("0", 0);
         put("I", 1);
         put("II", 2);
         put("III", 3);
@@ -28,82 +30,35 @@ public class Main {
 
     }};
 
-    public static void converter(String str) {
+
+    public static String converter(String str) {
 
         data = str.split("(?=[-+*/])|(?<=[-+*/])");
+        if(data.length>4){
+            throw new IllegalArgumentException("В вырожении не может быть больше двух чисел");
+        }
 
         try {
+
             a = Integer.parseInt(data[0]);
             b = Integer.parseInt(data[2]);
-            if (a > 11 || b > 11){
-                throw new ArithmeticException("числа не могут быть больше 10");
-            }
+
         } catch (NumberFormatException e) {
+            try{
 
             a = m.get(data[0]);
             b = m.get(data[2]);
+
             res = true;
-        }
-        // System.out.println(a + " " + b);
-
-    }
-
-
-    public static void converterResult(int result) {
-
-        StringBuilder romanResult = new StringBuilder(" ");
-
-
-        int hundreds = result / 100;
-        int tens = (result % 100) / 10;
-        int ones = result % 10;
-
-
-        if (tens == 5) {
-            romanResult.append("L");
-        } else if (tens == 9) {
-            romanResult.append("XC");
-        } else if (tens == 4) {
-            romanResult.append("XL");
-        } else if (hundreds == 1) {
-            romanResult.append("C");
-        } else if (tens == 6) {
-            romanResult.append("LX");
-        } else if (tens == 7) {
-            romanResult.append("LXX");
-        } else if (tens == 8) {
-            romanResult.append("LXXX");
-        } else if (tens > 0 && tens != 5 && tens != 4 && tens != 9) {
-            for (int i = 0; i < tens; i++) {
-                romanResult.append("X");
+        }catch (NullPointerException es){
+                throw new NullPointerException("Неверное значение");
             }
         }
 
-        for (Map.Entry<String, Integer> map : m.entrySet()) {
-            String key = map.getKey();
-            Integer value = map.getValue();
-            if (ones == value) {
-                romanResult.append(key);
-            }else if(ones == -value){
-                throw new ArithmeticException("Римские числа не могут быть отрицательными");
-            }
+
+        if(a > 11 || b > 11 || a < 1 || b < 1 ){
+           throw new NullPointerException("Неверное значение");
         }
-        if(romanResult.length() == 0){
-            romanResult.append("0");
-        }
-        System.out.println(romanResult);
-
-    }
-
-
-    public static void main(String[] args) {
-        System.out.println("Введите выражение");
-        String str = console.nextLine();
-
-
-        converter(str);
-
-        int result = 0;
 
         switch (data[1]) {
             case "+":
@@ -124,12 +79,73 @@ public class Main {
             default:
 
         }
+
         if (res) {
-            converterResult(result);
+            return converterResult(result);
 
         } else {
-            System.out.println(result);
+            return str = Integer.toString(result);
         }
+
+    }
+
+
+    public static String converterResult(int result) {
+
+        StringBuilder romanResult = new StringBuilder(" ");
+
+
+        int hundreds = result / 100;
+        int tens = (result % 100) / 10;
+        int ones = result % 10;
+
+
+        if (tens == 5) {
+            romanResult.append("L");
+        } else if (tens == 9 ) {
+            romanResult.append("XC");
+        } else if (tens == 4) {
+            romanResult.append("XL");
+        } else if (hundreds == 1) {
+            romanResult.append("C");
+        } else if (tens == 6) {
+            romanResult.append("LX");
+        } else if (tens == 7) {
+            romanResult.append("LXX");
+        } else if (tens == 8) {
+            romanResult.append("LXXX");
+        } else if (tens > 0 && tens != 5 && tens != 4 && tens != 9) {
+            for (int i = 0; i < tens; i++) {
+                romanResult.append("X");
+            }
+        }
+
+        if(ones != 0) {
+
+
+            for (Map.Entry<String, Integer> map : m.entrySet()) {
+                String key = map.getKey();
+                Integer value = map.getValue();
+                if (ones == value) {
+                    romanResult.append(key);
+                } else if (ones == -value) {
+                    throw new ArithmeticException("Римские числа не могут быть отрицательными");
+                }
+            }
+            if (romanResult.length() == 0) {
+                romanResult.append("0");
+            }
+        }
+        return romanResult.toString();
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Введите выражение");
+        String str = console.nextLine();
+
+        System.out.println(converter(str));
 
 
     }
